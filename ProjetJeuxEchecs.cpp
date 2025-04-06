65b773d60dd3fb28975abaac9fe9e8089e1cc913
noeju <noeju@MSI>
2025-04-06 15:49:09 -0400

    Add project files.

---
+++ ProjetJeuxEchecs.cpp
@@ -0,0 +1,17 @@
+﻿#include "ProjetJeuxEchecs.h"
+
+ProjetJeuxEchecs::ProjetJeuxEchecs(QWidget* parent)
+	: QMainWindow(parent)
+	, ui(new Ui::ProjetJeuxEchecsClass())
+{
+	ui->setupUi(this);
+
+
+
+	//...
+}
+
+ProjetJeuxEchecs::~ProjetJeuxEchecs()
+{
+	delete ui;
+}
---
+++ ProjetJeuxEchecs.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "ui_ProjetJeuxEchecs.h"
+#include <QtWidgets/QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class ProjetJeuxEchecsClass; };
+QT_END_NAMESPACE
+
+class ProjetJeuxEchecs : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	ProjetJeuxEchecs(QWidget* parent = nullptr);
+	~ProjetJeuxEchecs();
+
+private:
+	Ui::ProjetJeuxEchecsClass* ui;
+};
+
+class Piece {
+public:
+	virtual ~Piece() = default;
+	virtual void deplacer(int x, int y) = 0;
+};
+
+class Roi : public Piece {
+
+};
+
+class Tour : public Piece {
+
+};
+
+class Cavalier : public Piece {
+
+};
+
+
